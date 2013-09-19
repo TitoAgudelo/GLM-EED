cb5fb2d895f5c6a6086e9ce746e7770142d16c12
Tito Agudelo <titoarturoagudelo@gmail.com>
2013-09-19 16:06:33 -0500

    changes in controllers for redirect to acount log

--- GLM-EED/Controllers/HomeController.cs
+++ GLM-EED/Controllers/HomeController.cs
@@ -10,22 +10,11 @@ namespace GLM_EED.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "";
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
     }
