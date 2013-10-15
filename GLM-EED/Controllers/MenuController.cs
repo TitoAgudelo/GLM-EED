dac96f2234bf007a79eb65f3604a2b0560f2e72a
Tito Agudelo <titoarturoagudelo@gmail.com>
2013-10-15 14:32:11 -0500

    menuLuchIntegrate

--- GLM-EED/Controllers/MenuController.cs
+++ GLM-EED/Controllers/MenuController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web.Mvc;
+
+using GLM_EED.Filters;
+using GLM_EED.Models;
 
 namespace GLM_EED.Controllers
 {
-    public class MenuController : ApiController
+    [Authorize]
+    public class MenuController : Controller
     {
+        //
+        // GET: /Menu/Home
+
+        [AllowAnonymous]
+        public ActionResult Home(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
 
     }
 }
